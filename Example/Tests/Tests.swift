f1cc1f336d6bb09118811670c2a201757be7c0ea
Aaron Greenberg <aaron@friendlycaptcha.com>
2024-11-21 09:12:56 -0800

    Add SDK tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -2,49 +2,95 @@
 
 import Quick
 import Nimble
+import WebKit
 import FriendlyCaptcha
 
-class TableOfContentsSpec: QuickSpec {
+class FriendlyCaptchaSpec: QuickSpec {
     override func spec() {
-        describe("these will fail") {
+        var handle: FriendlyCaptcha!
+        var window: UIWindow!
 
-            it("can do maths") {
-                expect(1) == 2
-            }
+        beforeEach {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            handle = FriendlyCaptcha(sitekey: "FCMKRFNE60ACKCDC")
 
-            it("can read") {
-                expect("number") == "string"
-            }
+            window.rootViewController = handle.Widget()
+            window.makeKeyAndVisible()
+        }
 
-            it("will eventually fail") {
-                expect("time").toEventually( equal("done") )
-            }
-            
-            context("these will pass") {
+        it("initializes") {
+            expect(handle.getState()) == .initial
+            expect(handle.getResponse()) == ""
+        }
 
-                it("can do maths") {
-                    expect(23) == 23
+        it("is unactivated") {
+            let expectation = QuickSpec.current.expectation(description: "unactivated")
+            handle.onStateChange { (event) in
+                if event.state == .unactivated {
+                    expect(handle.getState()) == .unactivated
+                    expect(handle.getResponse()) == ".UNACTIVATED"
+                    expectation.fulfill()
                 }
+            }
+            QuickSpec.current.waitForExpectations(timeout: 5)
+        }
 
-                it("can read") {
-                    expect("🐮") == "🐮"
+        it("starts") {
+            let expectation = QuickSpec.current.expectation(description: "start")
+            handle.onStateChange { event in
+
+                // Wait to call handle.start() until the Widget has activated,
+                // to make sure we don't call it before the JavaScript has run.
+                if event.state == .unactivated {
+                    handle.start()
+                } else if event.state == .activated {
+                    expect(handle.getState()) == .activated
+                    expect(handle.getResponse()) == ".ACTIVATED"
+                    expectation.fulfill()
                 }
+            }
+            QuickSpec.current.waitForExpectations(timeout: 5)
+        }
 
-                it("will eventually pass") {
-                    var time = "passing"
+        it("resets") {
+            let expectation = QuickSpec.current.expectation(description: "reset")
+            handle.onStateChange { event in
 
-                    DispatchQueue.main.async {
-                        time = "done"
+                // Wait to call handle.start() until the Widget has activated,
+                // to make sure we don't call it before the JavaScript has run.
+                if event.state == .unactivated {
+                    handle.start()
+
+                // Then, after activation is done, try resetting.
+                } else if event.state == .activated {
+                    handle.reset()
+                } else if event.state == .reset {
+                    expect(handle.getState()) == .reset
+                    expect(handle.getResponse()) == ".RESET"
+                    expectation.fulfill()
+                }
+            }
+            QuickSpec.current.waitForExpectations(timeout: 5)
+        }
+
+        it("can be destroyed") {
+            expect(handle.Widget().viewIfLoaded).to(beAnInstanceOf(WKWebView.self))
+            let expectation = QuickSpec.current.expectation(description: "destroy")
+            handle.onStateChange { event in
+                if event.state == .unactivated {
+                    handle.start()
+                } else if event.state == .activated {
+                    handle.destroy()
+                } else if event.state == .destroyed {
+                    expect(handle.getState()) == .destroyed
+                    expect(handle.getResponse()) == ".DESTROYED"
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                        expect(handle.Widget().viewIfLoaded).to(beNil())
+                        expectation.fulfill()
                     }
-
-                    waitUntil { done in
-                        Thread.sleep(forTimeInterval: 0.5)
-                        expect(time) == "done"
-
-                        done()
-                    }
                 }
             }
+            QuickSpec.current.waitForExpectations(timeout: 5)
         }
     }
 }
