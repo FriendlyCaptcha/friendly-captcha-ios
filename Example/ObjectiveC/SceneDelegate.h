096333d618e36cc03a9e32c6be58688706b396e5
Aaron Greenberg <aaron@friendlycaptcha.com>
2024-11-25 17:04:16 -0800

    Add icons and splash screens

---
+++ Example/ObjectiveC/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  test
+//
+//  Created by Aaron Greenberg on 11/25/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
